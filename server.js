5c850d262e79db60a89c8f131f9d08da82f8068c
Aeyzaz Khan <aeyzaz@hotmail.com>
2019-10-07 23:58:02 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const path = require('path');
-//var secure = require('express-force-https')
+var secure = require('express-force-https')
 const port = process.env.PORT || 3001;
 const app = express();
 
 // serve static assets normally
-//app.use(secure);
+app.use(secure);
 app.use(express.static(__dirname + '/dist'));
 // handle every other route with index.html, which will contain
 // a script tag to your application's JavaScript file(s).
